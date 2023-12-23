89d91836a185dde585f095a625e8c9524477baf4
Connor <conkuljis@gmail.com>
2023-12-23 20:44:18 +0800

    update

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// NOTE: this code has been tested on macOS. It may or may not work on other platforms.
+
 package main
 
 import (
@@ -18,7 +20,8 @@ func main() {
 
 func kkyy() {
 	sequence := make([]KeyPress, 1000)
-	fmt.Println("Recording character sequence. Please input keys (press 'ENTER' to begin playback): ")
+	fmt.Println("Recording character sequence. Please input keys (press 'ENTER' " +
+		"to begin playback): ")
 
 	err := keyboard.Open()
 	if err != nil {
@@ -41,18 +44,18 @@ func kkyy() {
 			break
 		}
 
-		if key == keyboard.KeyBackspace2 {
+		if key == keyboard.KeyBackspace2 || key == keyboard.KeyBackspace {
 			if i == 0 {
 				continue
 			}
 
-			i--
-			// escape code to erase current line and move cursor to beginning.
-			fmt.Printf("\u001b[2K\r")
-			sequence[i] = KeyPress{} // effectively deletes an element
+			i = i - 1 // moves cursor back one position
+
+			clearTerminalLine()
 			for j := 0; j < i; j++ {
 				fmt.Printf("%c", sequence[j].Character)
 			}
+
 			continue
 		}
 
@@ -71,6 +74,7 @@ func kkyy() {
 	keyboard.Close()
 
 	debugRecording(sequence, i)
+	calculateWPM(sequence, i)
 }
 
 func NewKeyPress(char rune, currentTime, prevTime time.Time) KeyPress {
@@ -81,16 +85,38 @@ func NewKeyPress(char rune, currentTime, prevTime time.Time) KeyPress {
 	return KeyPress{Character: char, Delay: delay}
 }
 
-func debugRecording(recording []KeyPress, n int) {
-	for i := 0; i < n; i++ {
-		fmt.Printf("%d - %c - %s\n", i, recording[i].Character, recording[i].Delay.String())
-	}
-}
-
 func playbackRecording(recording []KeyPress, n int) {
 	for i := 0; i < n; i++ {
 		time.Sleep(recording[i].Delay)
 		fmt.Printf("%c", recording[i].Character)
 	}
 	fmt.Printf("\n")
+}
+
+func clearTerminalLine() {
+	// escape code to erase current line and move cursor to beginning.
+	fmt.Printf("\u001b[2K\r")
+}
+
+func debugRecording(recording []KeyPress, n int) {
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("\nDebug information:")
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d - %c - %s\n", i, recording[i].Character, recording[i].Delay.String())
+	}
+}
+
+func calculateWPM(recording []KeyPress, n int) {
+	var totalTime time.Duration
+	for i := 0; i < n; i++ {
+		totalTime = totalTime + recording[i].Delay
+	}
+
+	wordCount := float32(n) / 5
+
+	wpm := wordCount / float32(totalTime.Minutes())
+
+	fmt.Println("word count: ", wordCount)
+	fmt.Println("total time: " + totalTime.String())
+	fmt.Printf("WPM: %.f\n", wpm)
 }
