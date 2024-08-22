fcaef45824f81caea11cbfa4e7784eb2071ad67d
connorkuljis <conkuljis@gmail.com>
2024-08-22 14:58:05 +0800

    [seek] update makefile

--- seek/scripts/build.sh
+++ seek/scripts/build.sh
@@ -1,2 +1,8 @@
+#!/bin/zsh
+set -e 
+set -x
+
 docker buildx build --platform linux/amd64 -t connorkuljis/seek:latest .
 docker push connorkuljis/seek:latest
+
+gcloud run deploy seek-test-1 --image 'docker.io/connorkuljis/seek' --allow-unauthenticated --set-env-vars=GEMINIAPIKEY="$GEMINIAPIKEY"
