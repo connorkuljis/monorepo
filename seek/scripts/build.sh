e63b1e02a7334a28ecea3bed8a5204676394621e
connorkuljis <conkuljis@gmail.com>
2024-08-24 00:11:07 +0800

    remove scripts and static

--- seek/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-set -e 
-set -x
-
-docker buildx build --platform linux/amd64 -t connorkuljis/seek:latest .
-docker push connorkuljis/seek:latest
-
-gcloud run deploy seek-test-1 --image 'docker.io/connorkuljis/seek' --allow-unauthenticated --set-env-vars=GEMINIAPIKEY="$GEMINIAPIKEY"
